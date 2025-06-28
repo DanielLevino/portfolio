390f4a7ac2486b1f10d3859f594cd25928f4d243
Daniel Levino <engenheiro.levino@gmail.com>
2025-06-28 13:10:48 -0300

    mobile adjustment

--- js/script.js
+++ js/script.js
@@ -5,9 +5,6 @@ let navbar = document.querySelector('.navbar');
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
-console.log(navbar.classList);
-
-
 window.onscroll = () => {
     sections.forEach(sec => {
 
